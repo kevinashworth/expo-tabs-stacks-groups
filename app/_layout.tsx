696cb2dbdf324777b42d9ebbe34edaccf67e83e4
Kevin Ashworth <kevinashworth@yahoo.com>
2025-03-10 18:04:52 -0700

    Use a title to avoid "(tabs)" as back button

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -34,7 +34,13 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen
+          name="(tabs)"
+          options={{
+            headerShown: false,
+            // title: "Back",
+          }}
+        />
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
